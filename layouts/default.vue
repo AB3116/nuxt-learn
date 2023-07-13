8298489002f54ec42371138a67b50cea23ffd5fd
Apurva <apurvabhavsar31@gmail.com>
2023-07-13 21:12:27 +0530

    first commit

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-    <main class="p-12 bg-slate-800 text-slate-100 w-full h-full min-h-screen">
+    <main class="p-12 bg-red-800 text-slate-100 w-full h-full min-h-screen">
         <slot />
     </main>
 </template>
